166a27af9fc7168808e8a6d2472ffe3826c2bb6f
jasitharan <jasitharan1998@gmail.com>
2021-12-09 20:56:34 +0530

    d

--- src/components/FinalBalance.js
+++ src/components/FinalBalance.js
@@ -1,5 +1,4 @@
-import React, { useContext } from 'react'
-import { GlobalContext } from '../context/GlobalState';
+import React from 'react'
 // import { numberWithCommas } from '../utils/format';
 
 export const FinalBalance = () => {
