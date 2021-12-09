0f1b209076a73ffed189f10638c3904d94b25ec3
jasitharan <jasitharan1998@gmail.com>
2021-12-09 20:44:38 +0530

    commit

--- src/components/FinalBalance.js
+++ src/components/FinalBalance.js
@@ -3,21 +3,7 @@ import { GlobalContext } from '../context/GlobalState';
 // import { numberWithCommas } from '../utils/format';
 
 export const FinalBalance = () => {
-  const { transactions } = useContext(GlobalContext);
 
-  // const units = transactions.map(transaction => transaction.units);
-  // const quantity = units
-  //   .reduce((acc, item) => (acc += item), 0);
-  //const wacunits = transactions.map(transaction => transaction.wacunits);
- // const quantity = wacunits.length === 0 ? 0:wacunits[wacunits.length -1];
-
- //const wacPrices = transactions.map(transaction => transaction.wacuprice);
-  // const unitPrice = wacPrices.length === 0 ? 0:wacPrices[wacPrices.length -1];
-
-  // const value = transactions.map(transaction => transaction.value);
-  // const total = (value.reduce((acc, item) => (acc += item), 0)).toFixed(2);
-// const wacBalance = transactions.map(transaction => transaction.wacvalue);
- // const balance = wacBalance.length === 0 ? 0:wacBalance[wacBalance.length -1];
   var eoq = 0;
 
   const [demand, setDemand] = React.useState("");
